f9791f26893c260cd7f8adaf565030d9c76dba47
Srilakshminarayanau <161863396+Srilakshminarayanau@users.noreply.github.com>
2024-12-24 08:02:49 +0000

    commit 8

---
+++ app/src/main/java/uk/ac/tees/mad/myholidays/models/CalendrificResponse.kt
@@ -0,0 +1,42 @@
+package uk.ac.tees.mad.myholidays.models
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+data class CalendarificResponse(
+    @SerializedName("response") val response: HolidayResponse
+)
+
+data class HolidayResponse(
+    @SerializedName("holidays") val holidays: List<HolidayData>
+)
+
+data class HolidayData(
+    @SerializedName("name") val name: String,
+    @SerializedName("description") val description: String? = null,
+    @SerializedName("date") val date: HolidayDate,
+    @SerializedName("type") val type: List<String>? = null,
+    @SerializedName("locations") val locations: String? = null
+)
+
+data class HolidayDate(
+    @SerializedName("iso") val isoDate: String,
+    @SerializedName("datetime") val datetime: DateTimeDetails
+)
+
+data class DateTimeDetails(
+    @SerializedName("year") val year: Int,
+    @SerializedName("month") val month: Int,
+    @SerializedName("day") val day: Int
+)
+
+interface CalendarificApiService {
+    @GET("holidays")
+    suspend fun getHolidays(
+        @Query("api_key") apiKey: String,
+        @Query("country") country: String,
+        @Query("year") year: Int,
+        @Query("type") type: String = "national,religious"
+    ): CalendarificResponse
+}
