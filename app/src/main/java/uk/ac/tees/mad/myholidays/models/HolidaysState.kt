65ea86562d1c4ae0a7c8ff909a9d9d8d330c758f
Srilakshminarayanau <161863396+Srilakshminarayanau@users.noreply.github.com>
2024-12-29 01:36:01 +0000

    commit 10

--- app/src/main/java/uk/ac/tees/mad/myholidays/models/HolidaysState.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.myholidays.models
-
-sealed class HolidaysState {
-    object Initial: HolidaysState()
-    object Loading : HolidaysState()
-    data class Success(val holidays: List<Holiday>) : HolidaysState()
-    data class Error(val message: String) : HolidaysState()
-}
